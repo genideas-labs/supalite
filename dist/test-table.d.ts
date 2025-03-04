a963bef1a921794e1f51819e622308befc9323c6
Wondong Shin <wodshin@gmail.com>
2025-03-04 21:28:16 +0900

    ...

--- dist/test-table.d.ts
+++
@@ -1,5 +0,0 @@
-import { SupaLitePG } from './postgres-client';
-export declare function insertIntoTestTable(client: SupaLitePG<any>, name: string, value: number): Promise<any>;
-export declare function getFromTestTable(client: SupaLitePG<any>, conditions: {
-    [key: string]: any;
-}): Promise<any>;
